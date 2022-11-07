89fd989ca41a1a872c9643354f95bfd24ef62c3c
AndersMeinicke <andersmeinicke@gmail.com>
2022-11-07 09:53:58 +0100

    pom

--- src/main/java/dto/UserDTO.java
+++ src/main/java/dto/UserDTO.java
@@ -1,6 +1,11 @@
 package dto;
-
+import org.mindrot.jbcrypt.BCrypt;
 public class UserDTO {
     String username;
+    String password;
 
+    public UserDTO(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
